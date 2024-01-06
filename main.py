e2a0a56cba5e1142567844f3a9e7dac4c719b835
macarious <63441014+macarious@users.noreply.github.com>
2024-01-06 11:17:21 -0800

    Split data into train/valid/test

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if len(sys.argv) < 2:
     print("Please provide the speaker ID and the number of epochs (optional).")
     sys.exit(1)
   
-speaker_id = sys.argv[1]
+test_speaker = sys.argv[1]
 if len(sys.argv) == 3:
     if sys.argv[2].isdigit() and int(sys.argv[2]) > 0:
         num_epochs = int(sys.argv[2])
@@ -36,7 +36,7 @@ else:
     num_epochs = 30
 
 print()
-print("Speaker ID: ", speaker_id)
+print("Test Speaker: ", test_speaker)
 print("Number of epochs: ", num_epochs)
 print()
 
@@ -48,4 +48,31 @@ Read the Torgo dataset CSV file and store the data in a dictionary.
 '''
 data_df = pd.read_csv(torgo_csv_path)
 dataset_csv = load_dataset('csv', data_files=torgo_csv_path)
-print("Dataset CSV: ", dataset_csv)
+speakers = data_df['speaker_id'].unique()
+
+print()
+print("Unique speakers found in the dataset:")
+print(", ".join(speakers))
+
+if test_speaker not in speakers:
+    print("Test Speaker not found in the dataset.")
+    sys.exit(1)
+    
+print()
+
+
+'''
+--------------------------------------------------------------------------------
+Split the dataset into training / validation / test sets.
+--------------------------------------------------------------------------------
+'''
+valid_speaker = 'F03' if test_speaker != 'F03' else 'F04'
+train_speaker = [s for s in speakers if s not in [test_speaker, valid_speaker]]
+
+torgo_dataset = DatasetDict()
+torgo_dataset['train'] = dataset_csv['train'].filter(lambda x: x in train_speaker, input_columns=['speaker_id'])
+torgo_dataset['validation'] = dataset_csv['train'].filter(lambda x: x == valid_speaker, input_columns=['speaker_id'])
+torgo_dataset['test'] = dataset_csv['train'].filter(lambda x: x == test_speaker, input_columns=['speaker_id'])
+
+print(torgo_dataset)
+
