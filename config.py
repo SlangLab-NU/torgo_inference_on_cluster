1a2a95ac1c8fbb4de2741581a671ad7e90efbce2
macarious <63441014+macarious@users.noreply.github.com>
2024-01-06 23:11:16 -0800

    Preprocess the dataset

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-# Path to Torgo Dataset
-torgo_dataset_path = 'H:/My Drive/Torgo'
-torgo_csv_path = "H:/My Drive/torgo.csv"
+'''
+This file contains the paths to the Torgo dataset and the Torgo CSV file. This is used
+in finetuning the model on the Torgo dataset.
+'''
+# torgo_dataset_path = 'H:/My Drive/Torgo/' # Path to directory containing Torgo dataset
+# torgo_csv_path = "H:/My Drive/torgo.csv" # Path to directory containing Torgo CSV file
 
+torgo_dataset_path = '/mnt/h/My Drive/Torgo/' # Path to directory containing Torgo dataset
+torgo_csv_path = "/mnt/h/My Drive/torgo.csv" # Path to directory containing Torgo CSV file
